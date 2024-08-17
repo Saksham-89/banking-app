74db5a147c297b0ba03a2efcefe60204f1671411
Saksham <s.s.birla@student.utwente.nl>
2024-08-17 13:33:20 +0200

    homepage and sidebars initial implementation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,35 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalance from '@/components/TotalBalance'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = {firstName: "Saksham", lastName: "Birla", email: "email@email.com"}
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || "Guest"}
+                    subtext="Access and Manage your account"
+                />
+
+                <TotalBalance
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1000.00}
+                />
+            </header>
+            RECENT TRANSACTIONS
+        </div>
+        <RightSidebar
+            user={loggedIn}
+            transactions={[]}
+            banks={[{currentBalance: 123.50}, {currentBalance: 50.76}]}
+        />
+    </section>
   )
 }
 
